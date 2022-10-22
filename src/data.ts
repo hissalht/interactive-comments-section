6579bce05a2b4b9d6bd4037f726d94aec099e4ac
Adrien Boutigny <adrien@slash-tmp.dev>
2022-10-23 01:52:50 +0200

    add some cypress tests

--- src/data.ts
+++ src/data.ts
@@ -27,8 +27,10 @@ function generateComment(numberOfReplies: number): IComment {
 
 const comments: IComment[] = [];
 
-for (let i = 0; i < 5; i++) {
-  comments.push(generateComment(faker.datatype.number({ min: 0, max: 3 })));
+if (import.meta.env.VITE_APP_SEED_FAKE_DATA) {
+  for (let i = 0; i < 5; i++) {
+    comments.push(generateComment(faker.datatype.number({ min: 0, max: 3 })));
+  }
 }
 
 export default {
