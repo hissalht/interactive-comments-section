f77eabe61fa5ba98ed911545f50c508c96f58484
Adrien Boutigny <adrien@slash-tmp.dev>
2025-09-30 04:33:10 +0200

    upgrade faker

--- scripts/generate-fake-data.js
+++ scripts/generate-fake-data.js
@@ -7,8 +7,8 @@ import fs from "node:fs/promises";
 
 function generateComment(numberOfReplies) {
   const comment = {
-    id: faker.datatype.uuid(),
-    score: faker.datatype.number({ min: -3, max: 10 }),
+    id: faker.string.uuid(),
+    score: faker.number.int({ min: -3, max: 10 }),
     content: faker.lorem.paragraph(),
     createdAt: faker.date.recent().toISOString(),
     replies: [],
@@ -18,7 +18,7 @@ function generateComment(numberOfReplies) {
         png: faker.image.avatar(),
         webp: faker.image.avatar(),
       },
-      username: faker.internet.userName(),
+      username: faker.internet.username(),
     },
   };
 
@@ -32,7 +32,7 @@ function generateComment(numberOfReplies) {
 const comments = [];
 
 for (let i = 0; i < 5; i++) {
-  comments.push(generateComment(faker.datatype.number({ min: 0, max: 3 })));
+  comments.push(generateComment(faker.number.int({ min: 0, max: 3 })));
 }
 
 const currentUser = {
@@ -40,7 +40,7 @@ const currentUser = {
     png: faker.image.avatar(),
     webp: faker.image.avatar(),
   },
-  username: faker.internet.userName(),
+  username: faker.internet.username(),
 };
 
 fs.writeFile(
